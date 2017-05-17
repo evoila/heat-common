271b0972e6dad1a2baf036b660d102279de72baa
Mathias Ewald <mewald@evoila.de>
2017-05-17 10:58:39 +0200

    Enable all tests in run-tests.sh

--- run-tests.sh
+++ run-tests.sh
@@ -6,8 +6,7 @@ IMAGE=$3
 FLAVOR=$4
 PUBLIC_NETWORK=$5
 
-for TEST in prepare-volume; do 
-#for TEST in copy-file hosts-file java prepare-volume; do 
+for TEST in copy-file hosts-file java prepare-volume; do 
  openstack stack create -t test/$TEST.yaml\
   -e test/lib/heat-iaas/resources.yaml\
   -e resources-$DISTRIBUTION.yaml\
