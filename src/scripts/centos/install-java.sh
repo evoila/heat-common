6f02be42888453709853d09618214975ad193a5c
Nenad Radojevic <nradojevic@evoila.de>
2017-05-23 12:15:52 +0200

    Fix: Java installation via EPEL

--- src/scripts/centos/install-java.sh
+++ src/scripts/centos/install-java.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-curl -L -k \
-  -o /tmp/jdk-8u121-linux-x64.rpm \
-  -H "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie"\
-  http://download.oracle.com/otn-pub/java/jdk/8u121-b13/e9e7ea248e2c4826b92b3f075a80e441/jdk-8u121-linux-x64.rpm
-
-yum localinstall -y /tmp/jdk-8u121-linux-x64.rpm
+yum install -y java-1.8.0-openjdk.x86_64
