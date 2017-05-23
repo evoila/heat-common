93c99a5036487c2903a52d8fbf3033d073d9cfaa
Nenad Radojevic <nradojevic@evoila.de>
2017-05-23 19:28:34 +0200

    Fix: Differentiate file and folder permissions
    Add file and folder permissions
    
    Both still pointing to 664 by default

--- src/scripts/copy-file.sh
+++ src/scripts/copy-file.sh
@@ -5,7 +5,8 @@ FILENAME=${FILENAME}
 
 OWNER=${OWNER:-root}
 GROUP=${GROUP:-root}
-PERMS=${PERMS:-664}
+FILE_PERMS=${FILE_PERMS:-664}
+DIR_PERMS=${DIR_PERMS:-664}
 
 CONTENT=${CONTENT}
 
@@ -14,11 +15,11 @@ if [ ! -d $BASE_DIR ]; then
   mkdir -p $BASE_DIR
 fi
 chown $OWNER:$GROUP $BASE_DIR
-chmod $PERMS $BASE_DIR
+chmod $DIR_PERMS $BASE_DIR
 
 # Write file and set owner and permissions
 cat <<EOF > $BASE_DIR/$FILENAME
 $CONTENT
 EOF
 chown $OWNER:$GROUP ${BASE_DIR}/${FILENAME}
-chmod $PERMS $BASE_DIR/$FILENAME
+chmod $FILE_PERMS $BASE_DIR/$FILENAME
