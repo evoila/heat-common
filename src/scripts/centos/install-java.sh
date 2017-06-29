1255e5b85f231ff0ca7192434680cce1eb10fe7e
Mathias Ewald <mewald@evoila.de>
2017-06-29 08:59:48 +0200

    Fix: Broken Curl Command

--- src/scripts/centos/install-java.sh
+++ src/scripts/centos/install-java.sh
@@ -3,9 +3,8 @@
 OUTPUT_FILE="/tmp/jdk-linux-x64.rpm"
 
 curl -L -k\
- -o $OUTPUT_FILE\ 
+ -o $OUTPUT_FILE\
  -H "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie"\
  "http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.rpm"
-
 
 yum localinstall -y $OUTPUT_FILE
