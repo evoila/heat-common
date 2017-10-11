d4c79446c65266ca143757cd24202c85240d01dd
Mathias Ewald <mewald@evoila.de>
2017-10-11 14:00:30 +0200

    Configurable Volume Creation
    Make volume preparation deployment more flexible in terms of file system type,
    devices and mountpoints.

--- src/scripts/prepare-volume.sh
+++ src/scripts/prepare-volume.sh
@@ -1,24 +1,19 @@
 #!/bin/bash
 
+DEVICE=${DEVICE:-"/dev/vdb"}
 MOUNTPOINT=${MOUNTPOINT:-/mnt}
-FILESYSTEM=${FILESYSTEM:-ext4}
+FILESYSTEM_TYPE=${FILESYSTEM:-ext4}
 FILESYSTEM_LABEL=${FILESYSTEM_LABEL:-volume}
-
-# Find volume device file
-ROOT_DISK=$(cat /proc/mounts | grep -E "/dev/[vhs]d[a-z][0-9] /" | awk '{ print $1 }' | sed 's/[0-9]$//')
-VOLUME_DISK=$(fdisk -l | grep -E "Disk /dev/[hsv]d[a-z]" | grep -v "$ROOT_DISK" | head -n1 | awk '{ print $2 }' | sed 's/://')
+FILESYSTEM_OPTIONS=${FILESYSTEM_OPTIONS:-""}
 
 # Prepare file system 
-mkfs.$FILESYSTEM -L $FILESYSTEM_LABEL $VOLUME_DISK
-FS_UUID=$(blkid | grep "$VOLUME_DISK" | awk '{ print $3 }' | cut -d "=" -f 2 | sed 's/"//g')
+mkfs.$FILESYSTEM_TYPE -L $FILESYSTEM_LABEL $FILESYSTEM_OPTIONS $DEVICE
+FS_UUID=$(blkid | grep "$DEVICE" | awk '{ print $3 }' | cut -d "=" -f 2 | sed 's/"//g')
 
-if [ ! -d $MOUNTPOINT ]; then
-  mkdir -p $MOUNTPOINT
-fi
-
-mount $VOLUME_DISK $MOUNTPOINT
+mkdir -p $MOUNTPOINT
+mount $DEVICE $MOUNTPOINT
 
 # Make mount persistent
 cat <<EOF >> /etc/fstab
-UUID=${FS_UUID} $MOUNTPOINT ext4 defaults,errors=remount-ro 0 1
+UUID=${FS_UUID} $MOUNTPOINT $FILESYSTEM_TYPE defaults,errors=remount-ro 0 1
 EOF
