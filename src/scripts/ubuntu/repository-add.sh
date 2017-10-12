66fb02abd25b6114feb26528a728f62c1a7be47c
Felix Fischer <mail@felixfischer.org>
2017-10-12 14:39:21 +0200

    replaced quotes, removed unnecessary exports, refined test skript

--- src/scripts/ubuntu/repository-add.sh
+++ src/scripts/ubuntu/repository-add.sh
@@ -1,9 +1,5 @@
 #!/bin/bash -xe
 
-export REPOSITORY_ENTRY=${REPOSITORY_ENTRY}
-export REPOSITORY_PUBLICKEY=${REPOSITORY_PUBLICKEY}}
-export REPOSITORY_NAME=${REPOSITORY_NAME}
-
 echo "${REPOSITORY_PUBLICKEY}" | apt-key add -
 echo ${REPOSITORY_ENTRY} > /etc/apt/sources.list.d/${REPOSITORY_NAME}.list
 apt-get update
